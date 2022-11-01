38085d22732c1a42509086d1d90803132c85e0e8
Erick de Bem Campos <erickcamposbem69@gmail.com>
2022-10-31 22:34:34 -0300

    Ajustes Desingn sem função de autentificação

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import "../src/index.css"
+
 
 function App() {
+const handleClickButtonLogin = async() => {};
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen flex justify-center items-center">
+      <div className="bg-indigo-700 pt-20 pb-20 pl-20 pr-20 rounded-3xl">
+      <div className="flex flex-col items-center py-12">
+        <h1>Erick</h1>
+        <h3>O homem mais sortudo desse mundo!</h3>
+        <p>Tenho a melhor mulher que eu poderia pedir a Deus do meu lado!</p>
+        <p>Quer saber como ser sortudo assim? então só tenta porque conseguir vai ser dificil....</p>
+        <h2>ELA É UNICA!</h2>
+      </div>
+      <div>
+        <form className="flex flex-col items-center">
+          <h3>Faça o registro e boa tentativa</h3>
+        <p>mas ja aviso que não vai conseguir!</p>
+        <button onClick={() => handleClickButtonLogin }>Login Google</button>
+        </form>
+        
+      </div>
+      </div>
     </div>
   );
 }
